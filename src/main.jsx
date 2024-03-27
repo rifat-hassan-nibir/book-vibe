9a29e08314906fc207217060bd123b86889d5ce4
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-03-27 18:00:43 +0600

    custom shape bar chart added

--- src/main.jsx
+++ src/main.jsx
@@ -56,6 +56,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <ToastContainer />;
+    <ToastContainer />
   </React.StrictMode>
 );
--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -1,7 +1,45 @@
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
 const PagesToRead = () => {
+  const readBooksData = JSON.parse(localStorage.getItem("readBooks"));
+  console.log(readBooksData);
   return (
-    <div>
-      <h1>pages to read</h1>
+    <div className="flex justify-center">
+      <ResponsiveContainer width={1200} height={500}>
+        <BarChart
+          data={readBooksData}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="bookName" />
+          <YAxis dataKey="totalPages" />
+          <Tooltip></Tooltip>
+          <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: "top" }}>
+            {readBooksData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
